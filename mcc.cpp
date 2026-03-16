1698d35af4a45eb38fc97b704d16a217cc460e58
nathank07 <nathank7256@gmail.com>
2026-03-16 14:02:28 -0500

    make mcc only process one expression per file

--- mcc.cpp
+++ mcc.cpp
@@ -28,8 +28,25 @@ int main(int argc, char** argv) {
 
     midOsProg prog{out};
 
+    bool has_one_expr = false;
+
     for (auto& node : nodes) {
         if (!node.is_error()) {
+            if (has_one_expr) {
+                std::cout << "Found more than one expression <";
+                auto& expr_span = (*node)->token_span;
+                auto& first = expr_span.front();
+                auto& last = expr_span.back();
+                for (std::size_t ln = first.line_number; ln <= last.line_number; ++ln) {
+                    auto line = l->get_char_buff().get_line(ln);
+                    if (!line) continue;
+                    std::size_t start = (ln == first.line_number) ? first.column_number - 1 : 0;
+                    std::size_t end = (ln == last.line_number) ? last.column_number - 1 + last.get_token_width() : line->size();
+                    std::cout << line->substr(start, end - start);
+                }
+                std::cout << "> omitting..." << std::endl;
+            }
+            has_one_expr = true;
             midosGenerator::generate(prog, std::move(*node));
         } else {
             node.map_err([&](auto&& err) {
